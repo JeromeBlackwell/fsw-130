776b812ae1b9e4057ad648ab8d98b3a11f55624c
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-06-01 00:48:52 -0400

    week2

---
+++ week2/contextpractice2-app/src/GameShowInfo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { GameShowContextConsumer } from './GameShowContext.js'
+import './GameShowApp.css'
+
+function Show() {
+    return (
+        <GameShowContextConsumer>
+            {context => ( 
+                context.gameShow.map((gameShowInfo, index) => 
+                    <div className='gameShows' id={index} key={gameShowInfo}>
+                        <div className='td'>{gameShowInfo.title}</div>
+                        <div className='td'>{gameShowInfo.director}</div>
+                        <div className='td'>{gameShowInfo.genre}</div>
+                        <div className='td'>{gameShowInfo.year}</div>
+                    </div>
+                )
+            )}
+        </GameShowContextConsumer>
+    )
+}
+
+export default Show
---
+++ week2/contextpractice2-app/src/GameShowheading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './GameShowApp.css'
+
+function Show() {
+    return (
+        <div className='game'>
+            <div className='th'>GameShow Title</div>
+            <div className='th'>Director</div>
+            <div className='th'>Genre</div>
+            <div className='th'>Year</div>
+        </div>
+    )
+}
+
+export default Show
